78869afabaa4515cd5096ea9dd3b68c3d2158852
acoville <adam.coville@gmail.com>
2020-05-20 13:08:14 +0930

    initial commit of test directory and CMake list

---
+++ test/build/CMakeFiles/structures_unit_test.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/structures_unit_test.dir/test/testrunner.cpp.o"
+  "structures_unit_test"
+  "structures_unit_test.pdb"
+  "structures_unit_test_tests-b858cb2.cmake"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/structures_unit_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ test/build/structures_unit_test_tests-b858cb2.cmake
@@ -0,0 +1,1 @@
+set( structures_unit_test_TESTS)
